d9fd493f719f09508ed17697a79367a78d9e22ed
Lengieng <ing.lengieng@gmail.com>
2016-04-19 16:03:32 -0300

    Add bootstrap3 showcase
    - Add tabs
    - Add responsive table
    - Add contacts table

---
+++ showcase.js
@@ -0,0 +1,27 @@
+/** ******  collapse panel  *********************** **/
+// Close ibox function
+$('.close-link').click(function () {
+    var content = $(this).closest('div.x_panel');
+    content.remove();
+});
+
+// Collapse ibox function
+$('.collapse-link').click(function () {
+    var x_panel = $(this).closest('div.x_panel');
+    var button = $(this).find('i');
+    var content = x_panel.find('div.x_content');
+    content.slideToggle(200);
+    (x_panel.hasClass('fixed_height_390') ? x_panel.toggleClass('').toggleClass('fixed_height_390') : '');
+    (x_panel.hasClass('fixed_height_320') ? x_panel.toggleClass('').toggleClass('fixed_height_320') : '');
+    button.toggleClass('fa-chevron-up').toggleClass('fa-chevron-down');
+    setTimeout(function () {
+        x_panel.resize();
+    }, 50);
+});
+/** ******  /collapse panel  *********************** **/
+
+/** ******  progressbar  *********************** **/
+if ($(".progress .progress-bar")[0]) {
+    $('.progress .progress-bar').progressbar(); // bootstrap 3
+}
+/** ******  /progressbar  *********************** **/
